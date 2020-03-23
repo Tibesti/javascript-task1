352a73ed9da6311ceae64fc08756065e06188260
Tibesti <51659129+Tibesti@users.noreply.github.com>
2020-03-23 17:14:19 -0600

    Add files via upload

---
+++ javascript_task1.js
@@ -0,0 +1,12 @@
+let name = 'Tobiloba Olugbemi';
+let courses = ['HTML', 'CSS', 'Javascript', 'Nodejs'];
+console.log('My name is ' + name);
+console.log(courses);
+//since my number of courses is an even number
+const numbers = [];
+for (i=1; i<=200; i++){
+    if(i%2===0){
+        numbers.push(i);
+    }
+}
+console.log(numbers);
